33d0b30b21d3b6541b47c06eed10dcebbf83ebba
loi.huynhphuc <loi.huynhphuc@ncc.asia>
2023-01-18 15:43:42 +0700

    #1112: remove h1 "loi huynh phuc testing 1"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component,ViewChild,ElementRef ,OnInit, ViewEncapsulation} from '@angular/core';
 import { DateComponent } from './date/date.component';
 import { Location } from '@angular/common';
-import { Observable,Observer,of,from } from 'rxjs';
-import { filter, finalize, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -27,21 +25,9 @@ export class AppComponent {
     this.ageBox.nativeElement.value = currentYear - birthYear;
   }
 
-  myObservable = from([1,2,3,4,6])
-  transform =this.myObservable.pipe(
-    finalize(()=>{console.log('finalize')}),
-    function test(value){ 
-      return value},
-    map((value)=>{ return value * 5},
-    (value: number)=>{
-     return value + 5;
-    }),
-    filter((value)=>value >= 15),
-  ).pipe((value)=>value)
+ 
   ngOnInit() :void{
-      this.transform.subscribe(value=>{
-        console.log(value);
-      })
+     
   }
   title = 'angular-store-courses';
 }
